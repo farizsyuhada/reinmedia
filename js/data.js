250dc555ba424574b9659fe068172975a618f9cf
farizsyuhada <farizsyuhada01@gmail.com>
2026-01-18 10:56:21 +0700

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,65 @@
+const products = [
+  {
+    id: 1,
+    name: "Myiwe Natural Black",
+    brand: "Myiwe",
+    color: "Natural Black",
+    desc: "Warna hitam natural, pekat, namun tetap mengkilap.",
+    image: "assets/images/myiwe-natural-black.jpg",
+    link: "https://s.shopee.co.id/1gCOtqgk11"
+  },
+  {
+    id: 2,
+    name: "Myiwe Black Blue",
+    brand: "Myiwe",
+    color: "Black Blue",
+    desc: "Memberikan kesan hitam kebiruan yang trendy dan elegan.",
+    image: "assets/images/myiwe-black-blue.jpg",
+    link: "https://s.shopee.co.id/1gB0XnzOLD"
+  },
+  {
+    id: 3,
+    name: "Myiwe Brown",
+    brand: "Myiwe",
+    color: "Brown",
+    desc: "Warna coklat natural yang memberi dimensi elegan.",
+    image: "assets/images/myiwe-brown.jpg",
+    link: "https://s.shopee.co.id/9zq8vUj4q0"
+  },
+  {
+    id: 4,
+    name: "Myiwe Dark Brown",
+    brand: "Myiwe",
+    color: "Dark Brown",
+    desc: "Coklat gelap yang lembut dan tidak terlihat norak.",
+    image: "assets/images/myiwe-dark-brown.jpg",
+    link: "https://s.shopee.co.id/gIToXZ8nH"
+  },
+  {
+    id: 5,
+    name: "Aestika Black",
+    brand: "Aestika",
+    color: "Black",
+    desc: "Hitam pekat untuk tampilan natural dan bersih.",
+    image: "assets/images/aestika-black.jpg",
+    link: "https://s.shopee.co.id/7V8pqL7EpV"
+  },
+  {
+    id: 6,
+    name: "Aestika Light Brown",
+    brand: "Aestika",
+    color: "Light Brown",
+    desc: "Coklat terang yang memberikan kesan fresh.",
+    image: "assets/images/aestika-light-brown.jpg",
+    link: "https://s.shopee.co.id/3Vch3qcZBd"
+  },
+  {
+    id: 7,
+    name: "Aestika Dark Brown",
+    brand: "Aestika",
+    color: "Dark Brown",
+    desc: "Coklat gelap elegan dengan hasil natural.",
+    image: "assets/images/aestika-dark-brown.jpg",
+    link: "https://s.shopee.co.id/5Akv2egsSs"
+  }
+];
